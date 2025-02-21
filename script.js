45da48afe75df3b2c59c46aecb17293a1490197a
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-02-21 21:25:41 +0800

    feat: add function to display all books in UI

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ function addBookToLibrary(title, author, pages) {
   myLibrary.push(newBook);
 }
 
+const container = document.querySelector('.container');
+
+function displayBooks() {
+  for (const book of myLibrary) {
+    const card = document.createElement('p');
+    card.textContent = `${book.title} by: ${book.author} Pages: ${book.pages}`;
+    container.appendChild(card);
+  }
+}
+
 addBookToLibrary('Atomic Habits', 'James Clear', 346);
+addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 295);
+addBookToLibrary('Pride and Prejudice', 'Jane Austen', 429);
 addBookToLibrary('Test Book', 'Author of Doom', 142);
-console.log(myLibrary);
+
+displayBooks();
